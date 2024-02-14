050ae417654bc20b5b6a6168c69ba95c649e51de
amineguezmir <guezmiramine1@gmail.com>
2024-02-14 14:20:55 +0100

    contact , cursor animation , landing section , parallax, and the services are done (all almost done)

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,86 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Codini",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Dynamic e-learning platform offering interactive quizzes, engaging games, and comprehensive courses. Secure payment integration ensures seamless transactions. Enjoy access on both mobile and web platforms for flexible learning anytime, anywhere.",
+  },
+  {
+    id: 2,
+    title: "Hannibal",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Efficient e-commerce platform providing seamless browsing, secure transactions, and personalized user experiences. Explore a vast range of products with ease, enhanced by intuitive navigation and swift checkout processes.",
+  },
+  {
+    id: 3,
+    title: "BuyMe All",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Streamlined e-commerce hub offering a diverse product range, intuitive navigation, and secure transactions. Elevate your shopping experience with seamless browsing and convenient checkout options.",
+  },
+  {
+    id: 4,
+    title: "Pintastic",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Discover and share inspiration effortlessly on our Pinterest-like platform. Explore an array of captivating content, curated to inspire and engage users. Seamlessly pin and organize your favorite finds with intuitive tools for personalized collections.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+            <button>Source Code</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,131 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 const Services = () => {
-  return <div>Services</div>;
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I specialize in enhancing your online presence
+          <br /> through web development
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>End-to-End</motion.b>{" "}
+            Ideas for
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{ color: "orange" }}>
+              Web Development
+            </motion.b>{" "}
+            Services .
+          </h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Full Stack Web Development</h2>
+          <p>
+            Leveraging a robust skill set in both front-end and back-end
+            technologies, I craft dynamic and responsive web solutions tailored
+            to your unique needs. From intuitive user interfaces using HTML,
+            CSS, and JavaScript to scalable server-side applications with
+            Node.js, and Others, I deliver end-to-end web development services
+            that elevate your online presence and drive business growth.
+          </p>
+          <button>Conatct Me</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Responsive Web Design</h2>
+          <p>
+            With a keen eye for detail and a passion for creating engaging user
+            experiences, I specialize in crafting responsive web designs that
+            adapt seamlessly across all devices and screen sizes. Utilizing
+            modern frameworks such as React.js and Angular, coupled with my
+            expertise in CSS preprocessors like Sass and Less, I ensure that
+            your website not only looks stunning but also performs flawlessly on
+            every platform.
+          </p>
+          <button>Conatct Me</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Database Management</h2>
+          <p>
+            Data lies at the heart of every successful web application, and I
+            excel in designing and implementing robust database solutions to
+            efficiently manage and organize your information. Whether it's
+            relational databases like MySQL and PostgreSQL or NoSQL databases
+            such as MongoDB and Firebase, I leverage my expertise to optimize
+            data storage, retrieval, and manipulation, ensuring the scalability
+            and performance of your application.
+          </p>
+          <button>Conatct Me</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>E-commerce Solutions</h2>
+          <p>
+            Transforming your business into a thriving online marketplace, I
+            specialize in developing custom e-commerce solutions that streamline
+            the buying experience for your customers and empower you to manage
+            your products and sales effectively. Leveraging frameworks like
+            Django and Express.js, coupled with payment gateways like Stripe and
+            PayPal, I deliver secure and scalable e-commerce platforms that
+            drive conversions and maximize revenue.
+          </p>
+          <button>Conatct Me</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Services;
